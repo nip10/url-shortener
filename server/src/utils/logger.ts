0f9260bfb8b4dadf3be597d80f8fd9959f5f2f1d
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-14 19:57:37 +0100

    temp

---
+++ server/src/utils/logger.ts
@@ -0,0 +1,20 @@
+import winston, { createLogger } from 'winston';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { NODE_ENV } = process.env;
+const isProd = NODE_ENV === 'production';
+
+const logger = createLogger({
+  transports: [
+    new (winston.transports.Console)({ level: isProd ? 'info' : 'debug' }),
+    new (winston.transports.File)({ filename: 'debug.log', level: 'debug' })
+  ]
+});
+
+if (!isProd) {
+  logger.debug('Logging initialized at debug level');
+}
+
+export default logger;
