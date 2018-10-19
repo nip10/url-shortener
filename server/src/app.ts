bb659db08970842473c7711eb6f6a6eadd720776
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-19 21:21:01 +0100

    Fix logger and cleanup promise debugging

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,13 +14,13 @@ const { PORT, NODE_ENV } = process.env;
 const PORT_N = Number.parseInt(PORT, 10);
 
 if (!_.isFinite(PORT_N)) {
-  logger.log('error', 'You need to set a PORT in the .env file');
-  process.exit(1);
+  logger.error('You need to set a PORT in the .env file');
+  process.exit(-1);
 }
 
 if (_.isNil(NODE_ENV)) {
-  logger.log('error', 'You need to set a NODE_ENV in the .env file');
-  process.exit(1);
+  logger.error('You need to set a NODE_ENV in the .env file');
+  process.exit(-1);
 }
 
 const isProd = NODE_ENV === 'production';
@@ -50,21 +50,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
 app.listen(PORT_N, () => {
   logger.info(`Started on port ${PORT_N} in ${NODE_ENV} mode`);
-});
-
-// NOTE: event name is camelCase as per node convention
-process.on("unhandledRejection", (reason, promise) => {
-  console.log('unhandledRejection');
-  console.log('reason: ', JSON.stringify(reason, null, 2));
-  console.log('promise: ', JSON.stringify(promise, null, 2));
-  // See Promise.onPossiblyUnhandledRejection for parameter documentation
-});
-
-// NOTE: event name is camelCase as per node convention
-process.on("rejectionHandled", (promise) => {
-  console.log('rejectionHandled');
-  console.log('promise: ', JSON.stringify(promise, null, 2));
-  // See Promise.onUnhandledRejectionHandled for parameter documentation
 });
 
 export default app;
