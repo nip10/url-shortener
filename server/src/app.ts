8f3cbff28c53862fdc270fd1ce9d2c3da7721e3a
Diogo Cardoso <diogocardoso92@outlook.com>
2018-10-20 13:49:12 +0100

    Move app init to its own file
    And fix nodemon paths to point at it

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,23 +6,8 @@ import helmet from 'helmet';
 
 import urls from './routes/index';
 
-import logger from './utils/logger';
-
 dotenv.config();
-
-const { PORT, NODE_ENV } = process.env;
-const PORT_N = Number.parseInt(PORT, 10);
-
-if (!_.isFinite(PORT_N)) {
-  logger.error('You need to set a PORT in the .env file');
-  process.exit(-1);
-}
-
-if (_.isNil(NODE_ENV)) {
-  logger.error('You need to set a NODE_ENV in the .env file');
-  process.exit(-1);
-}
-
+const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === 'production';
 
 const app = express();
@@ -46,10 +31,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     return next(err);
   }
   return res.status(500).json({ error: isProd ? 'Server error' : err });
-});
-
-app.listen(PORT_N, () => {
-  logger.info(`Started on port ${PORT_N} in ${NODE_ENV} mode`);
 });
 
 export default app;
---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import dotenv from 'dotenv';
+import logger from './utils/logger';
+import app from './app';
+
+dotenv.config();
+
+const { PORT, NODE_ENV } = process.env;
+const PORT_N = Number.parseInt(PORT, 10);
+
+if (!_.isFinite(PORT_N)) {
+  logger.error('You need to set a PORT in the .env file');
+  process.exit(-1);
+}
+
+if (_.isNil(NODE_ENV) || !_.isString(NODE_ENV)) {
+  logger.error('You need to set a NODE_ENV in the .env file');
+  process.exit(-1);
+}
+
+app.listen(PORT_N, () => {
+  logger.info(`Started on port ${PORT_N} in ${NODE_ENV} mode`);
+});
