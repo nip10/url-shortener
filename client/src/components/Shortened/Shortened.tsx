baf7792390ed5b60d405b177ea40ae0eb534c690
Diogo Cardoso <diogocardoso92@outlook.com>
2018-12-16 16:12:58 +0000

    Add share feature and update styles

--- client/src/components/Shortened/Shortened.tsx
+++ client/src/components/Shortened/Shortened.tsx
@@ -1,21 +1,59 @@
 import React from 'react';
+import { faFacebookF, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './Shortened.css';
 
 const isDev = process.env.NODE_ENV === "development";
 const BASE_URL = isDev ? "http://localhost:3005/" : "https://sh.diogocardoso.me/";
 
-export default function Shortened({ url }: { url: string }) {
+interface IShortenedProps {
+  url: string,
+  elId: string,
+}
+
+const buildFullUrl = (url: string) => `${BASE_URL}${url}`;
+
+export default function Shortened({ url, elId }: IShortenedProps) {
+
+  const onCopyClickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (document.queryCommandSupported('copy')) {
+      const el = document.getElementById(elId);
+      if (el) {
+        const selection = window.getSelection();
+        const range = document.createRange();
+        range.selectNodeContents(el);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand("copy");
+      }
+    }
+  }
+
+  const onShareFacebookClickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const facebookWindow = window.open(`https://www.facebook.com/sharer/sharer.php?u=${buildFullUrl(url)}`, 'facebook-popup', 'height=350,width=600');
+    if (facebookWindow && facebookWindow.focus) { facebookWindow.focus(); }
+  }
+
+  const onShareTwitterClickHandler = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const twitterWindow = window.open(`https://twitter.com/share?url=${encodeURIComponent(buildFullUrl(url))}`, 'twitter-popup', 'height=350,width=600');
+    if (twitterWindow && twitterWindow.focus) { twitterWindow.focus(); }
+  }
+
   return (
     <div className="shortened__container">
       <div className="shortened__container-url">
-        <a target="_blank" rel="noopener noreferrer" href={`${BASE_URL}${url}`}>
-          {`${BASE_URL}${url}`}
+        <a id={elId} target="_blank" rel="noopener noreferrer" href={buildFullUrl(url)}>
+          {buildFullUrl(url)}
         </a>
       </div>
       <div className="shortened__container-share">
-        <p>share</p>
-        <p>share</p>
-        <p>copy to clipboard</p>
+        <div onClick={onShareFacebookClickHandler}><FontAwesomeIcon icon={faFacebookF}/></div>
+        <div onClick={onShareTwitterClickHandler}><FontAwesomeIcon icon={faTwitter}/></div>
+        <div onClick={onCopyClickHandler}><FontAwesomeIcon icon={faCopy}/><span>Copy to clipboard</span></div>
       </div>
     </div>
   )
