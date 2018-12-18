95b6ecd640c806ebf1ff8a709d8180db1de8ce77
Diogo Cardoso <diogocardoso92@outlook.com>
2018-12-18 00:34:05 +0000

    Remove css imports
    They are now in the App container

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
-import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
