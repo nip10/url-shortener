066a98d6d90e85363cbf589386e4ca3e4b697de5
Diogo Cardoso <diogocardoso92@outlook.com>
2019-03-12 22:40:11 +0000

    Fix ts error
    Added ICounterDocument to counter

--- server/src/models/url.ts
+++ server/src/models/url.ts
@@ -1,34 +1,43 @@
-import mongoose from './../config/db';
-import CounterSchema from './counter';
+import mongoose from "./../config/db";
+import CounterSchema, { ICounterDocument } from "./counter";
 
 export interface IUrlDocument extends mongoose.Document {
-  url: string,
+  url: string;
 }
 
-const UrlSchema = new mongoose.Schema({
-  _id: {
-    type: Number,
+const UrlSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: Number
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    hits: {
+      type: Number,
+      default: 0
+    }
   },
-  url: {
-    type: String,
-    required: true,
-  },
-  hits: {
-    type: Number,
-    default: 0,
+  {
+    timestamps: true
   }
-}, {
-    timestamps: true,
-  });
+);
 
-UrlSchema.pre('save', function(next) {
+UrlSchema.pre("save", function(next) {
   const foo = this;
-  CounterSchema.findByIdAndUpdate('linkEntryCount', { $inc: { count: 1 } }, { new: true, upsert: true },
-    (err, counter) => {
-      if (err) { return next(err); }
+  CounterSchema.findByIdAndUpdate(
+    "linkEntryCount",
+    { $inc: { count: 1 } },
+    { new: true, upsert: true },
+    (err, counter: ICounterDocument) => {
+      if (err) {
+        return next(err);
+      }
       foo._id = counter.count;
       return next();
-    });
+    }
+  );
 });
 
-export default mongoose.model<IUrlDocument>('Url', UrlSchema);
+export default mongoose.model<IUrlDocument>("Url", UrlSchema);
--- server/src/tests/seed/seed.ts
+++ server/src/tests/seed/seed.ts
@@ -1,18 +1,18 @@
-import Url from './../../models/url';
-import Counter from './../../models/counter';
-import logger from './../../utils/logger';
+import Url from "./../../models/url";
+import Counter, { ICounterDocument } from "./../../models/counter";
+import logger from "./../../utils/logger";
 
 export const urls = [
   {
-    _id : 1,
-    hits : 0,
-    url : 'http://www.google.com',
+    _id: 1,
+    hits: 0,
+    url: "http://www.google.com"
   },
   {
-    _id : 2,
-    hits : 3,
-    url : 'http://www.youtube.com',
-  },
+    _id: 2,
+    hits: 3,
+    url: "http://www.youtube.com"
+  }
 ];
 
 export const populateUrls = (done: jest.DoneCallback) => {
@@ -21,29 +21,39 @@ export const populateUrls = (done: jest.DoneCallback) => {
   return Promise.all([url1, url2])
     .then(() => done())
     .catch(err => logger.error("Error populating urls collection.", err));
-}
+};
 
 export const removeUrls = (done: jest.DoneCallback) => {
   Url.deleteMany({})
     .then(() => done())
     .catch(err => logger.error("Error deleting urls collection.", err));
-}
+};
 
+// linkEntryCount ID does not exist the first time it tries to update,
+// but the upsert option ensures it creates one the first time a new ID is used
 export const resetCounter = (done: jest.DoneCallback) => {
-  Counter.findByIdAndUpdate('linkEntryCount', { count: 0 }, { new: true, upsert: true},
-    (err, counter) => {
-      if (err) { throw new Error(err); }
-      if (counter.count !== 0) { throw new Error('Error updating counter value to 0.')}
+  Counter.findByIdAndUpdate(
+    "linkEntryCount",
+    { count: 0 },
+    { new: true, upsert: true },
+    (err, counter: ICounterDocument) => {
+      if (err) {
+        throw new Error(err);
+      }
+      if (counter.count !== 0) {
+        throw new Error("Error updating counter value to 0.");
+      }
       return done();
-    });
-}
+    }
+  );
+};
 
 export const countUrls = () => {
   return Url.countDocuments({})
-    .then((count) => count)
+    .then(count => count)
     .catch(err => logger.error("Error counting url documents.", err));
-}
+};
 
 // shortUrl '3' decodes to longUrl _id = 1
 // shortUrl '4' decodes to longUrl _id = 4
-export const shortUrls = ['3', '4'];
+export const shortUrls = ["3", "4"];
